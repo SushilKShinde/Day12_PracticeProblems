71b40462afbdaf4993f068e6f41c2c3158156393
Sushil <skshinde2028@gmail.com>
2023-05-05 18:51:51 +0530

    [Sushil] adding welcom message

---
+++ Day12_PracticeProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Day12_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 12 basic problems");
+            
+        }
+    }
+}
+
