89989dc4be54755f9173328ae62d7cb4f689df1b
Sushil <skshinde2028@gmail.com>
2023-05-05 22:31:14 +0530

    [Sushil] Reversing each word in sentence

--- Day12_PracticeProblem/Program.cs
+++ Day12_PracticeProblem/Program.cs
@@ -12,7 +12,7 @@ namespace Day12_PracticeProblem
         {
             Console.WriteLine("Welcome to Day 12 basic problems");
             Console.WriteLine("Choose the option from below");
-            Console.WriteLine("1.Find duplicate elements in array\n2.Print unique elements\n3.Maximum and minimun element in array");
+            Console.WriteLine("1.Find duplicate elements in array\n2.Print unique elements\n3.Maximum and minimun element in array\n4.Reverse each element in array");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -27,6 +27,9 @@ namespace Day12_PracticeProblem
                 case 3:
                     int[] arr1 = { 5, 8, 2, 10, 1, 7 };
                     MaxAndMinInArrayt.MinAndMax(arr1);
+                    break;
+                case 4:
+                    ReverseString.StringReverse();
                     break;
                 default:
                     Console.WriteLine("Choose valid option");
---
+++ Day12_PracticeProblem/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_PracticeProblem
+{
+    internal class ReverseString
+    {
+        public static void StringReverse()
+        {
+            string str = "Hello World";
+            Console.WriteLine($"Before Reverse: {str}");
+            Console.Write("\nAfter Reverse: ");
+            for (int i= str.Length-1; i>=0; i--)
+            {
+                for(int j=str.Length-1; j>=0; j--)
+                {
+                    Console.Write(str[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
